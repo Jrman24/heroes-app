b7c7ed8a7f959b8d24cc3ba08823b1d0f1ea86b9
Oscar M <manatajr@gmail.com>
2020-01-17 10:32:36 -0500

    Set up routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,29 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule } from '@angular/material/icon';
 import { ProductAlertsComponent } from './product-list/product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule} from '@angular/router';
+
 
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProductListComponent,
-    TopBarComponent,
-    ProductAlertsComponent,
-  ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    MatIconModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductListComponent,
+        TopBarComponent,
+        ProductAlertsComponent,
+        ProductDetailsComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatIconModule,
+        RouterModule.forRoot([
+            { path: '', component: ProductListComponent },
+            { path: 'products/:productId', component: ProductDetailsComponent },
+        ])
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
